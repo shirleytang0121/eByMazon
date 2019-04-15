5f931197f5ce54fdcd2e79ba9d1718e3692cbd2f
jinchen1036 <jinchen1036@users.noreply.github.com>
2019-04-14 23:16:40 -0400

    Edit DB schema, layout functions for GU,OU,SU,Item, and GeneralFunction

--- GeneralFunctions.py
+++ GeneralFunctions.py
@@ -3,7 +3,8 @@ class General():
         self.cursor = cursor
 
     def login_check(self, username, password):
-
+        # check login info with DB
+        # still need to implement for check OU status if is OU and GU application
         self.cursor.execute(
             "SELECT ID, userType FROM User WHERE username=%s, password=%s",
             (username,password))
@@ -20,3 +21,28 @@ class General():
 
 
 
+    def checkStaus(self, ouID):
+        # return status of the select OU
+        pass
+
+    def changeStaus(self, ouID):
+        #update new status
+        # get call when receive warning or low rating
+        pass
+
+    def manageWarnig(self,ouID):
+        # called when add a warning
+        # check number warning receive, if greater or equal to 2, suspend OU
+        # else make sure that ou is not VIP
+        pass
+
+    def ouBlacklist(self,ouID):
+        # remove the ou from all DB and all active sale item post by that ou,
+        # but not the sold item by he/she, the seller will be None
+        # add the username to ouBlacklist
+        pass
+
+    def itemBlacklist(self,itemID):
+        # remove all the occurance of this item in DB
+        # add to itBlacklist
+        pass
