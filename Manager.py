10f0bb158e329805719cf37da4d7e3434c9916e3
hongzhi pan <victoryhongzhi@gmail.com>
2019-04-20 18:36:11 -0400

    updated small modification to UI

--- Manager.py
+++ Manager.py
@@ -142,8 +142,8 @@ class ShowcaseApp(App):
 
 if __name__ == "__main__":
     config = {
-        "user": '',                 # Enter your own username
-        "password": '',             # Enter your own password
+        "user": 'eby',                 # Enter your own username
+        "password": 'ebypw',             # Enter your own password
         "host": '127.0.0.1',
         "database": 'eByMazon'
     }
